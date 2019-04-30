dd89c9df1e4c0da4f996677d037c75748620bfef
Caleb T. Vanorio <caleb.vanorio@centre.edu>
2019-04-30 15:31:50 -0400

    mapAndColor

--- makeMap.js
+++ makeMap.js
@@ -1,32 +1,80 @@
-var countiesP = d3.json("countiesData.json");
-countiesP.then(function(countyData)
+var stateP = d3.json("stateData.json");
+var cityCountyDataP = d3.csv("StateHateCrime2005.csv");
+var countyDataP = d3.json("countiesData.json");
+Promise.all([stateP,cityCountyDataP,countyDataP]).then(function(data)
 {
-  drawMap(countyData);
+  stateData = data[0];
+  cityCountyData = data[1];
+  countyData = data[2];
+  drawMap(stateData,cityCountyData);
 })
 
 
-var drawMap = function(countyData)
+var drawMap = function(outline,dataInMap)
 {
-  var width = 800;
+  // make the svg
+  var width = 1500;
   var height = 800;
   var body = d3.select("body");
   var svg = body.append("svg")
       .attr("width",width)
       .attr("height",height);
 
+  makeMapHaveData(outline,dataInMap);
+//projection of US
   var projection = d3.geoAlbersUsa()
-                    .translate(width/2,height/2);
+                    .scale([width])
+                    .translate([750,400]);
 
-  console.log(countyData.features);
+  console.log(outline);
   var countyGenerator = d3.geoPath().projection(projection);
-
-  counties = svg.selectAll("g")
-      .data(countyData.features)
+  //var colors = d3.interpolateReds();
+  counties = svg.append("g").selectAll("g")
+      .data(outline.features)
       .enter()
-      .append("g")
+      .append("path")
+      .attr("d",countyGenerator)
+      .attr("stroke","gray")
+      .attr("fill",function(d){
+        console.log(d.properties.Incidents/100)
+        return d3.interpolateReds(d.properties.IncidentsPer1000);
+      })
+      .on("mouseover",function(d){
+        console.log(d.properties.IncidentsNom);
+        svg.append("text")
+          .attr("transform","translate("+countyGenerator.centroid(d)[0]+","+countyGenerator.centroid(d)[1]+")")
+          .text(d.properties.IncidentsNom);
+      })
+      .on("mouseout",function(d){
+        svg.select("text")
+          .remove();
+      });
+}
 
-  counties.append("path")
-      .attr("d",countyGenerator);
-
-
+var makeMapHaveData = function(outline,dataInMap)
+{
+  console.log(dataInMap);
+  var curStateFeature;
+  // state is dictionary of state name to number of incidents nominally and per 1000 people
+  var state = {};
+  var stateList=[];
+  dataInMap.forEach(function(d){
+    if (d.State!="")
+    {
+      state[d.State] = [parseInt(d.Incidents.replace(",",""))/parseInt(d.Population.replace(",",""))*10];//parseInt(d.race)+parseInt(d.religion)+parseInt(d.sexualOrientation)+parseInt(d.disability)+parseInt(d.gender);
+      state[d.State].push(d.Incidents);
+      stateList.push(d.State);
+    }
+  })
+  console.log(state);
+  for (var i=0;i<outline.features.length;i++)
+  {
+     curStateFeature = outline.features[i];
+     if (stateList.indexOf(curStateFeature.properties.NAME)>=0)
+     {
+       console.log(curStateFeature);
+       curStateFeature.properties["IncidentsPer1000"] = state[curStateFeature.properties.NAME][0];
+       curStateFeature.properties["IncidentsNom"] = state[curStateFeature.properties.NAME][1];
+     }
+  }
 }
